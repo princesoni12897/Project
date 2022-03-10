73a9135a3e181c4f1719548a7c79420fd7f6b960
prince <princesoni12897@gmail.com>
2022-03-10 13:50:32 +0530

    BootMvc_01

---
+++ src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,1 @@
+<h1 style="color:red;text-align:center">Welcome Yo HomePage</h1>
---
+++ src/test/java/com/nt/BootMvCfirst01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMvCfirst01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
